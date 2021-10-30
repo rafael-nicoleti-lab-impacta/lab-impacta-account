8930e5d88020e1041b8da1de95e0f13da95e54d9
Rafael Nicoleti <rafaelnicolett@outlook.com>
2021-10-30 12:37:43 -0300

    Adicionado estrutura de aplication e controller

--- account/src/main/java/br/com/lab/impacta/account/service/impl/AccountServiceImpl.java
+++ account/src/main/java/br/com/lab/impacta/account/service/impl/AccountServiceImpl.java
@@ -1,6 +1,7 @@
 package br.com.lab.impacta.account.service.impl;
 
 import br.com.lab.impacta.account.handler.exception.AccountDontExistsException;
+import br.com.lab.impacta.account.handler.exception.AccountWithoutBalanceException;
 import br.com.lab.impacta.account.model.Account;
 import br.com.lab.impacta.account.repository.AccountRepository;
 import br.com.lab.impacta.account.service.AccountService;
@@ -25,6 +26,9 @@ public class AccountServiceImpl implements AccountService {
     @Value("${lab.account.exceptions.account-without-balance-message}")
     private String messageExceptionAccountWithoutBalance;
 
+    @Value("${lab.account.exceptions.account-without-balance-description}")
+    private String descriptionExceptionAccountWithoutBalance;
+
     @Override
     public Account findAccount(Long accountId) {
         Optional<Account> account = accountRepository.findById(accountId);
@@ -38,6 +42,14 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public void debitAccount(Long accountId, Double valueOfDebit) {
+        Account account = this.findAccount(accountId);
 
+        boolean debited = account.debit(valueOfDebit);
+
+        if (!debited)
+            throw new AccountWithoutBalanceException(messageExceptionAccountWithoutBalance,
+                    descriptionExceptionAccountWithoutBalance);
+
+        accountRepository.save(account);
     }
 }
